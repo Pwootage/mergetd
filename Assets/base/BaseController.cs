a1a606f47cd716dc0314ed4a5c6b64e4ef4e6b0b
Pwootage <pwootage@gmail.com>
2016-10-13 09:39:11 -0600

    Remove unused 3d model from base

--- Assets/base/BaseController.cs
+++ Assets/base/BaseController.cs
@@ -3,17 +3,10 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class BaseController : MonoBehaviour {
-	public Light light;
-	public GameObject ring;
 
 	void Start() {
 	}
 
 	void Update() {
-		float t = Time.realtimeSinceStartup * 5;
-		float x = Mathf.Cos(t);
-		float y = Mathf.Sin(t);
-		light.transform.localPosition = new Vector3(x * 0.15f, y * 0.15f, -0.4f);
-		ring.transform.localRotation = Quaternion.Euler(y * 30f - 90f, 0, 0) * Quaternion.Euler(new Vector3(0, x * 30f, 0));
 	}
 }
