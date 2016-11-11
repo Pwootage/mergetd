dd3093c5e67191d55259431f43e444334d0f3f4c
Ben Hawker <trovanhawk@gmail.com>
2016-11-10 23:10:03 -0700

    popup and other menu tweaks

---
+++ Assets/Menu/PopUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUp : MonoBehaviour {
+    public void Show() {
+        transform.SetAsLastSibling();
+    }
+
+    public void Hide() {
+        transform.SetAsFirstSibling();
+    }
+}
